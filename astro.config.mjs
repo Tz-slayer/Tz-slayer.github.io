85036f117725808f93ba04057835aae2c0594f37
Tz-slayer <q1554788327@gmail.com>
2025-08-20 23:30:52 +0800

    添加GitHub pages配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tz-slayer.github.io",
+	site: "https://Tz-slayer.github.io",
 
 	trailingSlash: "always",
 	integrations: [
